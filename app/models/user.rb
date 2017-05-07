820e8d2d39fb83ec3eabf48e36983041d5bbd316
jiro.kirameki <firebom.sns@gmail.com>
2017-05-07 12:27:09 +0000

    favorite, unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ApplicationRecord
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverses_of_relationship, source: :user  
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :fav_microposts, through: :favorites, source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -25,6 +28,21 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def favorite(other_micropost)
+    unless self == other_micropost
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+    end
+  end
+
+  def unfavorite(other_micropost)
+    fav = self.favorites.find_by(micropost_id: other_micropost.id)
+    fav.destroy if fav
+  end
+  
+  def favorite?(other_micropost)
+    self.fav_microposts.include?(other_micropost)
   end
   
   def feed_microposts
